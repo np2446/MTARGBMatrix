e2d803c19565bec4c2bf586ff21914721f0442a7
np2446 <95257872+np2446@users.noreply.github.com>
2023-06-10 15:58:24 -0400

    changed to display two lines at once

--- rgb.py
+++ rgb.py
@@ -16,22 +16,24 @@ class MatrixClass():
 
         # initialize fonts and canvas
         fontPath = "/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf"  # replace with desired font path
-        self.font = ImageFont.truetype(fontPath, size=24)
+        self.font = ImageFont.truetype(fontPath, size=12)
         self.canvas = Image.new("RGB", (self.options.cols, self.options.rows))
         self.draw = ImageDraw.Draw(self.canvas)
 
-    def displayText(self, text):
+    def displayText(self, text1, text2):
         self.matrix.Clear()
-        text_width, text_height = self.draw.textsize(text, font=self.font)
+        text_width, text_height = self.draw.textsize(text1, font=self.font)
         x = self.options.cols
-        y = 2
+        y1 = 0
+        y2 = 17
 
         while x > -text_width:
-            self.canvas = Image.new("RGB", (self.options.cols, self.options.rows))
+            self.canvas = Image.new("RGB", (self.options.cols, self.options.row>
             self.draw = ImageDraw.Draw(self.canvas)
-            self.draw.text((x, y), text, font=self.font, fill=(255, 0, 0))
+            self.draw.text((x, y1), text1, font=self.font, fill=(255, 255, 0))
+            self.draw.text((x,y2), text2, font=self.font, fill=(255,0,0))
             self.matrix.SetImage(self.canvas.convert('RGB'))
-            x -= 0.1  # Adjust the scrolling speed here
+            x -= 0.35  # Adjust the scrolling speed here
 
     
 
